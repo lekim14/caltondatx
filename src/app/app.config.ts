b241ee0046ad059467a0986221d68a4b8f5a4e2e
Joshua Mikel Jimenez <joshua.mikel.jimenez.neust.sic.cict@gmail.com>
2025-05-02 11:46:53 +0800

    fix theme

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,7 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { providePrimeNG } from 'primeng/config';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
-import Aura from '@primeng/themes/Aura';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
